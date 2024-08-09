09d19e34937af42c93b90dacc4fb65837d73d1d8
VERMAMANISHKUMAR <manishkumarverma091@gmail.com>
2024-08-09 17:49:14 +0530

    Function and class Components

---
+++ Week-02/React Components-1/classcomponent/src/App.js
@@ -0,0 +1,27 @@
+
+import React from 'react';
+//Class Component extends React.Component
+export default class App extends React.Component {
+  render() {
+    return (
+      <div >
+        <h1>This is Class Component</h1>
+      </div>
+    )
+  }
+}
+
+
+// Function Component
+// import React  from 'react';
+
+// export default function App(){
+//   return(
+    
+//     <>
+//       <h1>This is Function Component</h1>
+//     </>
+    
+//   )
+// }
+
